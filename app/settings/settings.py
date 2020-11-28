456f60b5d7b4d3d84e2c1526dc5e7701d4883ff3
physics-machinelearning <kuroki.ryo.physics@gmail.com>
2020-11-29 02:32:39 +0900

    fix

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/staticfolder/'
+STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfolder')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
