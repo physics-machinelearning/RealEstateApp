db7ce13defb501e22666853191806d7814de4e24
physics-machinelearning <kuroki.ryo.physics@gmail.com>
2020-12-09 12:07:20 +0900

    git checkout from test_on_local

--- app/realestate/forms.py
+++ app/realestate/forms.py
@@ -8,8 +8,20 @@ class SearchConditionForm(ModelForm):
         model = SearchCondition
         fields = ('min_rent', 'max_rent', 'floor_plan', 'min_area', 'bath_toilet', 'autolock')
         labels = {
-            'min_rent': '家賃下限(万円)',
-            'max_rent': '家賃上限(万円）',
+            'min_rent': '家賃下限',
+            'max_rent': '家賃上限',
+            'floor_plan': '間取り',
+            'min_area': '占有面積下限',
+            'bath_toilet': 'バストイレ',
+            'autolock': 'オートロック'
+        }
+
+
+class SearchConditionMapForm(ModelForm):
+    class Meta:
+        model = SearchCondition
+        fields = ('floor_plan', 'min_area', 'bath_toilet', 'autolock')
+        labels = {
             'floor_plan': '間取り',
             'min_area': '占有面積下限',
             'bath_toilet': 'バストイレ',
